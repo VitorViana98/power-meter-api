3d64c6befeb8ce7302e4eecddfe26dd19c86ff7a
Vitor Barbosa <vitor.barbosa@somagrupo.com.br>
2024-01-10 22:38:17 -0300

    fix: user

--- src/controllers/userCircuitController.js
+++ src/controllers/userCircuitController.js
@@ -10,7 +10,6 @@ const getCircuits = async (req, res) => {
   const { usuario } = req.query;
   try {
     const query = "SELECT * FROM power_metter.tb_circuits where user_id = ?;";
-    console.log('aqui usuario', usuario)
     const [results] = await db.query(query, [usuario[0].user_id]);
 
     return ok(res, results);
@@ -27,7 +26,7 @@ const createCircuit = async (req, res) => {
     !circuitName ||
     circuitName.length === 0 ||
     !usuario ||
-    !usuario.user_id
+    !usuario[0].user_id
   ) {
     return requiredField(res, "Invalid params");
   }
@@ -39,7 +38,7 @@ const createCircuit = async (req, res) => {
     const results = await db.query(query, [
       circuitName,
       circuitDescription,
-      usuario.user_id,
+      usuario[0].user_id,
     ]);
 
     res.status(201).json({
@@ -62,24 +61,12 @@ const getDashboard = async (req, res) => {
     return requiredField(res, "Invalid params");
   }
 
-  //   const query = `
-  //   SELECT currents.timestamp, currents.current_measurement, voltages.voltage_measurement,
-  //          (currents.current_measurement * voltages.voltage_measurement) AS power,
-  //          circuits.circuit_name, circuits.circuit_description
-  //   FROM power_metter.currents
-  //   LEFT JOIN power_metter.voltages ON currents.circuit_id = voltages.circuit_id AND currents.timestamp = voltages.timestamp
-  //   LEFT JOIN power_metter.tb_circuits AS circuits ON currents.circuit_id = circuits.circuit_id
-  //   WHERE currents.circuit_id = ?
-  //   ORDER BY currents.timestamp DESC;
-  // `;
-
   const query = `SELECT timestamp, current_measurement, voltage_measurement, power, circuit_name, circuit_description
   FROM (
     SELECT currents.timestamp, currents.current_measurement, voltages.voltage_measurement,
            (currents.current_measurement * voltages.voltage_measurement) AS power,
            circuits.circuit_name, circuits.circuit_description
     FROM power_metter.currents
-    LEFT JOIN power_metter.voltages ON currents.circuit_id = voltages.circuit_id AND currents.timestamp = voltages.timestamp
     LEFT JOIN power_metter.tb_circuits AS circuits ON currents.circuit_id = circuits.circuit_id
     WHERE currents.circuit_id = ?
     ORDER BY currents.timestamp DESC
@@ -89,6 +76,14 @@ const getDashboard = async (req, res) => {
 
   try {
     const [results] = await db.query(query, [circuitId]);
+
+    if (!results || !results.length > 0 || !results[0]?.circuit_name) {
+      return res.status(201).json({
+        content: {
+          message: "Circuit results not found",
+        },
+      });
+    }
 
     return res.status(201).json({
       content: {
